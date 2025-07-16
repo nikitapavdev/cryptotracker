8cbe49b5845dd704a2534a7d35e6ac2548354ee0
apronprof <nikita.pavlov.prof@gmail.com>
2025-07-16 05:16:15 +0200

    feat(backend): auth login method added

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\FileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,6 +14,8 @@ Route::get('/user', function (Request $request) {
 
 
 });*/
+
+Route::post('/login', [AuthController::class, 'login']);
 
 Route::get('/files/{file}/download', [FileController::class, 'download']);
 Route::post('/files/upload-url', [FileController::class, 'getUploadUrl']);
