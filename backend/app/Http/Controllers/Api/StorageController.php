48c90f131c94f57b7a9d6adcb3ea306abe82791f
apronprof <nikita.pavlov.prof@gmail.com>
2025-07-18 12:05:01 +0200

    feat(backend): added upload-url method & updated dependencies

---
+++ backend/app/Http/Controllers/Api/StorageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Aws\S3\S3Client;
+
+class StorageController extends Controller
+{
+    use AuthorizesRequests;
+
+    public function __construct(){
+        $this->middleware('auth:sanctum'); 
+    }
+
+    public function getUploadUrl(Request $request){
+
+        $validated = $request->validate([
+            'original_name' => 'required|string',
+            'mime_type' => 'required|string',
+        ]);
+
+        $disk = Storage::disk('s3');
+
+        $uniqueKey = Str::uuid()->toString();
+
+        $client = new S3Client([
+            'version' => 'latest',
+            'region' => config('filesystems.disks.s3.region'),
+            'endpoint' => config('filesystems.disks.s3.endpoint'),
+            'use_path_style_endpoint' => config('filesystems.disks.s3.use_path_style_endpoint', false),
+            'credentials' => [
+                'key' => config('filesystems.disks.s3.key'),
+                'secret' => config('filesystems.disks.s3.secret'),
+            ],
+        ]);
+
+        $bucket = config('filesystems.disks.s3.bucket');
+        $user_id = Auth::id();
+        $key = "$user_id/$uniqueKey";
+
+        $command = $client->getCommand('PutObject', [
+            'Bucket' => $bucket,
+            'Key' => $key,
+            'ContentType' => $validated['mime_type'],
+        ]);
+
+        $s3_request = $client->createPresignedRequest($command, '+10 minutes');
+
+        $url = (string) $s3_request->getUri();
+
+        return response()->json([
+            'upload_url' => $url,
+            'file_path' => $key,
+        ]);
+    }
+
+    public function download(Request $request){
+
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,26 +1,25 @@
 <?php
 
-use App\Http\Controllers\Api\FileController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Api\FileController;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\StorageController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-/*Route::middleware('auth:sanctum')->group(function () {
-
-
-
-});*/
-
+// Authentification
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout')->middleware(['auth:sanctum']);
 
-Route::get('/files/{file}/download', [FileController::class, 'download']);
-Route::post('/files/upload-url', [FileController::class, 'getUploadUrl']);
+// Files upload and download
+Route::get('/files/{file}/download', [StorageController::class, 'download']);
+Route::post('/files/upload-url', [StorageController::class, 'getUploadUrl']);
 
+// Resource paths
 Route::apiResource('files', FileController::class)->only([
     'index', 'store', 'show', 'update', 'destroy'
 ]);
